bac111b442e5a9b45e1b237b2c1db6cb7519525b
mengmeng0531 <dongchengren001@gmail.com>
2023-07-14 02:44:54 +0800

    login register

---
+++ meshop/meshop-products/src/main/java/com/sztus/meshop/products/repository/writer/ProductWriter.java
@@ -0,0 +1,16 @@
+package com.sztus.meshop.products.repository.writer;
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.github.yulichang.base.MPJBaseServiceImpl;
+import com.sztus.meshop.products.object.domain.Product;
+import com.sztus.meshop.products.repository.mapper.ProductMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author LuffyWang
+ */
+@Repository
+@DS("writer")
+public class ProductWriter extends MPJBaseServiceImpl<ProductMapper, Product> {
+
+}
--- meshop/meshop-user/src/main/java/com/sztus/meshop/user/object/domain/User.java
+++ meshop/meshop-user/src/main/java/com/sztus/meshop/user/object/domain/User.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+<<<<<<< HEAD
 import lombok.Data;
 
 @Data
@@ -24,4 +25,33 @@ public class User {
 
     @TableField("created_at")
     private Long createdAt;
+=======
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@TableName("user")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User implements Serializable {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private Integer position;
+    private String openId;
+    private String username;
+    private String email;
+    @TableField(exist = false)
+    private String password;
+    @TableField(exist = false)
+    private String confirmPassword;
+    private String telephone;
+    private Long birthday;
+    private String nickname;
+    private String gender;
+    private String avatarUrl;
+    private Long createdAt;
+    private Long updatedAt;
+    private Integer status;
+>>>>>>> e109ab7 (login register)
 }
